3a78e41d28afed97037bd6e076aff5928723a985
Cuauhtemoc Alejandro Herrera Perez <cuahutemoc.herrera@iteso.mx>
2026-09-24 22:15:24 -0600

    P1_End_Device

---
+++ P1_end_dev_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/mwa_end_device.h
@@ -0,0 +1,120 @@
+/*!
+* Copyright (c) 2014, Freescale Semiconductor, Inc.
+* Copyright 2016-2017 NXP
+* All rights reserved.
+*
+* \file
+*
+* This header file is for MyWirelessApp Demo End Device application.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*! *********************************************************************************
+*************************************************************************************
+* Include
+*************************************************************************************
+********************************************************************************** */
+
+/************************************************************************************
+*************************************************************************************
+* Public macros
+*************************************************************************************
+************************************************************************************/
+
+/* Equipo 7, nodo End Device - debe ser distinta a la del Coordinador */
+#define mMacExtendedAddress_c      (0x0000000000070002ULL)
+
+#ifndef gNvmTestActive_d
+#define gNvmTestActive_d           (0)
+#endif
+
+#ifndef mEnterLowPowerWhenIdle_c
+#define mEnterLowPowerWhenIdle_c (0)
+#endif
+
+#ifdef gPHY_802_15_4g_d
+  #define mDefaultValueOfChannel_c (0x0001FFFF)
+#else
+  /* Canal fijo para el Equipo 7: solo escanear el canal 11 */
+  #define mDefaultValueOfChannel_c (1 << 11)
+#endif
+
+/* Maximum number of outstanding packets */
+#define mDefaultValueOfMaxPendingDataPackets_c 2
+
+/* The slow polling interval is used if the coordinator
+   had no data last time we polled. */
+#define mDefaultValueOfPollIntervalSlow_c 820 /* ~(0.001*mDefaultValueOfPollIntervalSlow_c) secs */
+
+/* The fast polling interval is used if the coordinator had data last time we
+   polled, thus we increase the band-width while data is available. */
+#define mDefaultValueOfPollIntervalFast_c 82 /* ~(0.001*mDefaultValueOfPollIntervalFast_c) secs */
+
+#if gNvmTestActive_d
+  #define mDefaultValueOfTimeoutError_c 10
+#endif
+
+#if mEnterLowPowerWhenIdle_c
+  #define mDefaultValueOfMlmeHandlersToAllowSleep_c 30
+#endif
+
+/************************************************************************************
+*************************************************************************************
+* Public type definitions
+*************************************************************************************
+************************************************************************************/
+
+/* All states in the applications state machine */
+enum {
+  stateInit,
+  stateScanActiveStart,
+  stateScanActiveWaitConfirm,
+  stateAssociate,
+  stateAssociateWaitConfirm,
+  stateListen
+};
+
+/* Events that are passed to the application task. 
+   Are defined as byte masks to make possible 
+   send multiple events to the task */
+
+#define gAppEvtDummyEvent_c            (1 << 0)
+#define gAppEvtRxFromUart_c            (1 << 1)
+#define gAppEvtMessageFromMLME_c       (1 << 2)
+#define gAppEvtMessageFromMCPS_c       (1 << 3)
+#define gAppEvtPressedRestoreNvmBut_c  (1 << 4)
+/* Fase 1: timer de 4s del contador, y evento de override por switch */
+#define gAppEvtCounterTimerExpired_c   (1 << 5)
+#define gAppEvtSwitchOverride_c        (1 << 6)
+
+/* Error codes */
+enum {
+  errorNoError,
+  errorWrongConfirm,
+  errorNotSuccessful,
+  errorNoMessage,
+  errorAllocFailed,
+  errorInvalidParameter,
+  errorNoScanResults
+};
+
+/******************************************************************************
+*******************************************************************************
+* Public Prototypes
+*******************************************************************************
+******************************************************************************/
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/**********************************************************************************/
+#endif /* _APP_H_ */
